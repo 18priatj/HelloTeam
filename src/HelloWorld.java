6d01769b91e17f84ecc99667144f0c9a0b794d4b
Jasmine Priatna <priatnajasmine@gmail.com>
2018-02-16 13:26:26 -0600

    Jasmine's Movie

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
 System.out.println("Sanjana Tarigoppula");
+System.out.println("Guardians of the Galaxy");
 	}
 
 }
